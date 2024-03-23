a858635b69dea94fb5aa3840b53f40f548f326cc
Tom Voet <tomvoet@gmx.de>
2024-03-23 20:45:54 +0100

    init

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    https://cloud.google.com/vertex-ai/generative-ai/docs/start/quickstarts/quickstart-multimodal#gemini-beginner-samples-nodejs
+    https://console.cloud.google.com/vertex-ai/generative/multimodal/gallery?hl=de&project=foodforthemood
+    https://cloud.google.com/vertex-ai/generative-ai/docs/model-reference/gemini?hl=de#gemini-pro-vision
+    <NuxtPage />
+  </main>
+</template>
